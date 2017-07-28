2f234e57a7ecdecb8fb373e9b1442b2992459d88
Oleh Bondarenko <boa.st@ya.ru>
2017-07-28 14:32:44 +0300

    Update TextEditor.java

--- src/com/boast/textEditor/TextEditor.java
+++ src/com/boast/textEditor/TextEditor.java
@@ -1,29 +1,30 @@
+/*
+ * TextEditor.java 27/07/2017
+ *
+ * Created by Bondarenko Oleh
+ */
+
+
 package com.boast.textEditor;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Stack;
 
-/**
- * Created by Bondarenko Oleh on 26.07.2017.
- */
-
 public class TextEditor {
-    public static StringBuilder addAfter(StringBuilder text, String wordEnd, String wordToInsert) {
+    public static void addAfter(StringBuilder text, String wordEnd, String wordToInsert) {
         Pattern pattern = Pattern.compile(wordEnd + "(\\b)");
         Matcher matcher = pattern.matcher(text);
 
         Stack<Integer> foundIndexes = new Stack<>();
 
         while (matcher.find()) {
-            foundIndexes.push(matcher.end() - 1);
+            foundIndexes.push(matcher.end());
         }
 
         while (!foundIndexes.isEmpty()) {
             text.insert(foundIndexes.peek(), " " + wordToInsert);
             foundIndexes.pop();
         }
-
-        return text;
     }
 }
